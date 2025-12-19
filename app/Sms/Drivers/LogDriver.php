af753d67369fcdd870887fbf2a0d5b4795a01641
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2025-12-19 20:01:43 +0330

    refactor(auth): move OtpRequested event to Auth namespace

---
+++ app/Sms/Drivers/LogDriver.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Sms\Drivers;
+
+class LogDriver
+{
+
+}
---
+++ app/Sms/Interfaces/SmsDriverInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Exceptions\SmsSendingFailedException;
+
+interface SmsDriverInterface
+{
+    /**
+     * Send a plain text SMS to a single recipient.
+     * Suitable for general notifications (e.g., order status).
+     *
+     * @param string $mobile The recipient's mobile number.
+     * @param string $message The content of the message.
+     * @return string The message ID (for tracking delivery status).
+     * @throws SmsSendingFailedException
+     */
+    public function send(string $mobile, string $message): string;
+
+    /**
+     * Send an SMS using a pre-defined pattern (Lookup/Service).
+     * Highly recommended for OTP and high-priority alerts.
+     *
+     * @param string $mobile The recipient's mobile number.
+     * @param string $patternCode The specific pattern code defined in the SMS provider panel.
+     * @param array $params An associative array of variables to replace in the pattern (e.g., ['code' => '1234']).
+     * @return string The message ID.
+     * @throws SmsSendingFailedException
+     */
+    public function sendPattern(string $mobile, string $patternCode, array $params): string;
+
+    /**
+     * Send a single message to multiple recipients (Bulk).
+     *
+     * @param array $mobiles An array of mobile numbers.
+     * @param string $message The content of the message.
+     * @return array An array of message IDs.
+     * @throws SmsSendingFailedException
+     */
+    public function sendBulk(array $mobiles, string $message): array;
+}
