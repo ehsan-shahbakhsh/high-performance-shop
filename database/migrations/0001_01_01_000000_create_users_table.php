c132e7722f39903c6ebfcaa5b7143da6417c9036
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2025-12-21 21:40:24 +0330

    fix(database): make users email nullable

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('mobile', 15)->unique()->nullable()->index();
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->unique();
 
             $table->timestamp('mobile_verified_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
