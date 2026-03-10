7bbb41a933dc53bfac98919038f512e2b856db5b
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-03-10 15:34:11 +0330

    test(pest): add google social login helpers

---
+++ tests/Pest.php
@@ -0,0 +1,26 @@
+<?php
+
+function mockGoogleUser($id = 'google-123', $email = 'test@gmail.com'): void
+{
+    $googleUser = Mockery::mock(\Laravel\Socialite\Two\User::class);
+
+    $googleUser->shouldReceive('getId')->andReturn($id);
+    $googleUser->shouldReceive('getEmail')->andReturn($email);
+    $googleUser->shouldReceive('getAvatar')->andReturn('https://avatar.test');
+
+    $googleUser->shouldReceive('getRaw')->andReturn([
+        'given_name' => 'John',
+        'family_name' => 'Doe',
+    ]);
+
+    $googleUser->token = 'fake-google-token';
+
+    $provider = Mockery::mock(\Laravel\Socialite\Contracts\Provider::class);
+
+    $provider->shouldReceive('stateless')->andReturnSelf();
+    $provider->shouldReceive('user')->andReturn($googleUser);
+
+    Socialite::shouldReceive('driver')
+        ->with('google')
+        ->andReturn($provider);
+}
