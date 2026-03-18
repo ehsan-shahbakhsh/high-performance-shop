dd3021b585675be0c674f8bde35733d52c85d75d
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-03-18 08:16:14 +0330

    feat: define product and variant relationships on WishlistItem model

--- app/Models/WishlistItem.php
+++ app/Models/WishlistItem.php
@@ -14,8 +14,20 @@ class WishlistItem extends Model
 
     protected $fillable = ['wishlist_id', 'product_id', 'variant_id'];
 
+    protected $perPage = 20;
+
     public function wishlist(): BelongsTo
     {
         return $this->belongsTo(Wishlist::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function variant(): BelongsTo
+    {
+        return $this->belongsTo(ProductVariant::class, 'variant_id');
     }
 }
