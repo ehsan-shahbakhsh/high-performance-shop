185726f45adb59a218a564f0a8b390dd7a1f4031
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-04-07 01:18:59 +0330

    chore(seeder): register additional seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,10 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call([
             UserSeeder::class,
+            ProvinceSeeder::class,
+            CitySeeder::class,
+            IconSeeder::class,
+            TagSeeder::class,
+            AttributeSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
