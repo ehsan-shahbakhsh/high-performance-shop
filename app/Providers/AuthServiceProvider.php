4298e031f8fef78d946ac4c159baa7ae759172bf
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-04-12 18:46:55 +0330

    feat: add AddressPolicy and register in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,14 +2,9 @@
 
 namespace App\Providers;
 
-use App\Models\Cart;
-use App\Models\CartItem;
-use App\Models\Wishlist;
-use App\Models\WishlistItem;
-use App\Policies\Sales\CartItemPolicy;
-use App\Policies\Sales\CartPolicy;
-use App\Policies\Sales\WishlistItemPolicy;
-use App\Policies\Sales\WishlistPolicy;
+use App\Models\{Cart, CartItem, Wishlist, WishlistItem, Address};
+use App\Policies\Sales\{CartPolicy, CartItemPolicy, WishlistPolicy, WishlistItemPolicy};
+use App\Policies\Customer\AddressPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -19,5 +14,6 @@ class AuthServiceProvider extends ServiceProvider
         CartItem::class => CartItemPolicy::class,
         Wishlist::class => WishlistPolicy::class,
         WishlistItem::class => WishlistItemPolicy::class,
+        Address::class => AddressPolicy::class,
     ];
 }
