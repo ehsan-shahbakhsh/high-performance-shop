72359f7ca86f2867340df61964e9cdfdd8258520
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-04-17 19:24:16 +0330

    feat(coupons): set default sort to newest first

--- app/Filament/Resources/Coupons/Tables/CouponsTable.php
+++ app/Filament/Resources/Coupons/Tables/CouponsTable.php
@@ -15,6 +15,7 @@ class CouponsTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
                 ShopTable::id(),
 
