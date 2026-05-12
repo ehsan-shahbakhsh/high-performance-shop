0707f47b934e071cd0fc02ab058c4b67e88ae759
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-05-12 21:13:40 +0330

    fix(discount): set default amount to 0 for bogo discounts

--- app/Filament/Resources/Discounts/Pages/CreateDiscount.php
+++ app/Filament/Resources/Discounts/Pages/CreateDiscount.php
@@ -2,10 +2,21 @@
 
 namespace App\Filament\Resources\Discounts\Pages;
 
+use App\Enums\DiscountType;
 use App\Filament\Resources\Discounts\DiscountResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateDiscount extends CreateRecord
 {
     protected static string $resource = DiscountResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        if ($data['type'] == DiscountType::BuyXGetY->value) {
+            $data['amount'] = 0;
+        }
+
+        return parent::handleRecordCreation($data);
+    }
 }
