f5bc9128209bd3d0d76888fc719e8dd7a37cec20
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-05-27 05:26:41 +0330

    refactor: remove unused DateFormatted trait and update AuthUserResource

--- app/Http/Resources/V1/Auth/AuthUserResource.php
+++ app/Http/Resources/V1/Auth/AuthUserResource.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Resources\V1\Auth;
 
-use App\Http\Resources\Concerns\DateFormatted;
 use App\Http\Resources\V1\UserResource;
 use Illuminate\Http\Request;
 
 class AuthUserResource extends UserResource
 {
-    use DateFormatted;
-
     /**
      * Transform the resource into an array.
      *
@@ -22,12 +19,12 @@ class AuthUserResource extends UserResource
         return array_merge($publicData, [
             'mobile' => $this->mobile,
             'email' => $this->email,
-            'mobile_verified_at' => $this->toPersianDate($this->mobile_verified_at),
-            'email_verified_at' => $this->toPersianDate($this->email_verified_at),
-            'banned_at' => $this->toPersianDate($this->banned_at),
-            'last_login_at' => $this->toPersianDate($this->last_login_at),
+            'mobile_verified_at' => $this->mobile_verified_at,
+            'email_verified_at' => $this->email_verified_at,
+            'banned_at' => $this->banned_at,
+            'last_login_at' => $this->last_login_at,
             'last_login_ip' => $this->last_login_ip,
-            'created_at' => $this->toPersianDate($this->created_at),
+            'created_at' => $this->created_at,
         ]);
     }
 }
