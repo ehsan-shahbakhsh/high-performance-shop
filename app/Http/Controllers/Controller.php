c6632d30b9393d9a41dbd7f9bda7539de8686198
Ehsan <ehsan.shahbakhsh.email@gmail.com>
2026-07-19 13:27:30 +0330

    docs: setup base openapi documentation structure

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(version: "1.0.0", description: "مستندات جامع وب‌سرویس‌های فروشگاه", title: "High Performance Shop API")]
+#[OA\Server(url: "http://localhost:8000/api", description: "سرور اصلی")]
+#[OA\SecurityScheme(
+    securityScheme: "sanctum",
+    type: "apiKey",
+    description: "توکن احراز هویت (Bearer Token) خود را اینجا وارد کنید",
+    name: "Authorization",
+    in: "header"
+)]
 abstract class Controller
 {
     //
